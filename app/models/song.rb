58f70e837fddd059c9b54720eb5d2c76f9b4bff9
Desmond Rawls <captaingrover@gmail.com>
2013-07-07 21:13:02 -0400

    initial commit

---
+++ app/models/song.rb
@@ -0,0 +1,5 @@
+class Song < ActiveRecord::Base
+  attr_accessible :name
+  belongs_to :artist
+  belongs_to :genre
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PostTracker::Application.config.secret_token = '7b49a7fcf8a21f42c112db6d3f68a1b4d59f814e43dc5e37d042acd657866de94de0dd18f23ca5db76d534adbb10c2a8aaa9fe1697cd73a3d170b37a5d089811'
